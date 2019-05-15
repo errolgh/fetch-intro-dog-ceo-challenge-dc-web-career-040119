ebe7e674003596949cc32397eb1fe8a5c8d793ba
errolgh <ewatsonrei@gmail.com>
2019-05-15 16:13:44 -0400

    challenge 1 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', getAllDogs())
+
+function getAllDogs(){
+  fetch(imgUrl)
+  .then(res=>res.json())
+  .then(dogs => {
+    // debugger
+    dogs["message"].forEach(printer)
+  })
+}
+//DOM manipulation function
+function printer(dog){
+  let bigList = document.getElementById("dog-breeds")
+  // debugger
+  let dogEntry = document.createElement("img")
+  dogEntry.src = dog
+  // debugger
+  bigList.appendChild(dogEntry)
+}
