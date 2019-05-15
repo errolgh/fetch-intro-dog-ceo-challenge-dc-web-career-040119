03db0d735143fefe0e4476bc743d6c14db3ab698
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-15 16:37:35 -0400

    challenge 2 completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-document.addEventListener('DOMContentLoaded', getAllDogs())
+document.addEventListener('DOMContentLoaded', getAllThings())
+
+function getAllThings() {
+  getAllDogs()
+  getAllBreeds()
+}
 
 function getAllDogs(){
   fetch(imgUrl)
@@ -11,12 +16,34 @@ function getAllDogs(){
     dogs["message"].forEach(printer)
   })
 }
+
+function getAllBreeds() {
+  fetch(breedUrl)
+  .then(res => res.json())
+  .then(dogs => {
+    breedLogger(dogs)
+    })
+}
+
 //DOM manipulation function
 function printer(dog){
-  let bigList = document.getElementById("dog-breeds")
+  let bigList = document.getElementById("dog-image-container")
   // debugger
   let dogEntry = document.createElement("img")
   dogEntry.src = dog
   // debugger
   bigList.appendChild(dogEntry)
+}
+
+function breedLogger(dogs) {
+  for (const key in dogs["message"]) {
+    makeBreedList(key)
+  }
+}
+
+function makeBreedList(key) {
+  let bigMoneyMoves = document.getElementById("dog-breeds")
+  let li = document.createElement("li")
+  li.innerText = key
+  bigMoneyMoves.appendChild(li)
 }
